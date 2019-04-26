7b4879c894f12c19e2021ad064bf6589678f3f7a
StoyanMalinin <stoyanmalinin2002@gmail.com>
2019-04-26 18:38:58 +0300

    Create 2D_FenwickTree.cpp

---
+++ Data Structures/2D_FenwickTree.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+const int MAXN = 1005;
+
+struct fenwick1D
+{
+    int n;
+    long long tree[MAXN];
+
+    fenwick1D(){}
+    fenwick1D(int n)
+    {
+        this->n = n;
+        memset(this->tree, 0, sizeof(this->tree));
+    }
+
+    void update(int index, long long value)
+    {
+        if(index<=0) return;
+        while(index<=this->n)
+        {
+            this->tree[index] += value;
+            index += index&(-index);
+        }
+    }
+
+    long long getPrefix(int index)
+    {
+        long long result = 0;
+        while(index>0)
+        {
+            result += this->tree[index];
+            index -= index&(-index);
+        }
+
+        return result;
+    }
+
+    long long query(int l, int r)
+    {
+        return getPrefix(r)+getPrefix(l-1);
+    }
+};
+
+struct fenwick2D
+{
+    int n;
+    fenwick1D *tree[MAXN];
+
+    fenwick2D(){}
+    fenwick2D(int n, int m)
+    {
+        this->n = n;
+        for(int i = 0;i<=n+1;i++)
+        {
+            this->tree[i] = new fenwick1D(m);
+        }
+    }
+
+    void update(int x, int y, long long value)
+    {
+        if(x<=0) return;
+        while(x<=this->n)
+        {
+            this->tree[x]->update(y, value);
+            x += x&(-x);
+        }
+    }
+
+    long long query(int x, int y)
+    {
+        long long result = 0;
+        while(x>0)
+        {
+            result += this->tree[x]->getPrefix(y);
+            x -= x&(-x);
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+
+}
