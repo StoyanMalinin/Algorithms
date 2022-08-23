fa5b8f00f0e194a244e310dc43fee39963db36e6
StoyanMalinin <stoyanmalinin2002@gmail.com>
2022-08-23 19:21:58 +0300

    completed the solution to problem Acrobat

--- past problems/BOI/2016/Acrobat.cpp
+++ past problems/BOI/2016/Acrobat.cpp
@@ -1,13 +1,33 @@
+#include <algorithm>
 #include <iostream>
+#include <cstring>
+#include <cassert>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 const int MAXN = 3e5;
 
+struct Modification
+{
+    int type;
+    int i, j;
+
+    Modification(){}
+    Modification(int type, int i, int j) : type(type), i(i), j(j) {}
+};
+
+int n, m;
+bool parA[MAXN+5];
+multiset <pair <int, int>> ropes;
+
+bool used[2][MAXN+5];
+vector <int> graphA[MAXN+5];
+vector <int> graph2[2][MAXN+5];
+
 struct Forest
 {
-    bool isOdd[MAXN+5];
     vector <int> adj[MAXN+5];
 
     void addEdge(int u, int v)
@@ -16,30 +36,47 @@ struct Forest
         adj[v].push_back(u);
     }
 
-    int dfs(int x, int last, const int ODD_CNT)
+    int dfs(int x, int last, const int ODD_CNT, vector <pair <int, int>> &edges)
     {
         int subOddCnt = 0;
+        if(parA[x]==true) subOddCnt++;
 
+
+        for(int y: adj[x])
+        {
+            if(y!=last)
+            {
+                subOddCnt += dfs(y, x, ODD_CNT, edges);
+            }
+        }
+
+        if(last!=-1)
+        {
+            int par = (subOddCnt%2)*((ODD_CNT-subOddCnt)%2);
+            if(par==1)
+            {
+                if(ropes.count(make_pair(x, last))==true)
+                    edges.emplace_back(x, last);
+                else
+                    edges.emplace_back(last, x);
+            }
+        }
+
+        return subOddCnt;
     }
 };
 
-int n, m;
-pair <int, int> ropes[MAXN+5];
-
 Forest F;
-bool used[MAXN+5];
-vector <int> graphA[MAXN+5];
-
 int dfs1(int x)
 {
-    used[x] = true;
+    used[0][x] = true;
 
     int oddCnt = 0;
-    if(graphA[x].size()%2!=0) oddCnt++;
+    if(parA[x]==true) oddCnt++;
 
     for(int y: graphA[x])
     {
-        if(used[y]==false)
+        if(used[0][y]==false)
         {
             F.addEdge(x, y);
             oddCnt += dfs1(y);
@@ -49,22 +86,118 @@ int dfs1(int x)
     return oddCnt;
 }
 
+int dfs2(int side, int x, vector <int> &oddNodes)
+{
+    used[side][x] = true;
+    if(side==1 && graph2[1][x].size()%2!=0)
+        oddNodes.push_back(x);
+
+    int sz = 1;
+    for(int y: graph2[side][x])
+    {
+        if(used[side^1][y]==false)
+            sz += dfs2(side^1, y, oddNodes);
+    }
+
+    return sz;
+}
+
 int main()
 {
-    //ios::sync_with_stdio(false);
-    //cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector <Modification> ans;
 
     cin >> n >> m;
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i<m;i++)
     {
         int u, v;
         cin >> u >> v;
 
-        ropes[i] = make_pair(u, v);
+        ropes.emplace(u, v);
         graphA[u].push_back(v);
         graphA[v].push_back(u);
+        parA[u] ^= true;
     }
 
+    vector <pair <int, int>> edges;
     for(int x = 1;x<=n;x++)
-        F.isOdd[x] = (graphA[x].size()%2==1);
+    {
+        if(used[0][x]==false)
+        {
+            int oddCnt = dfs1(x);
+            F.dfs(x, -1, oddCnt, edges);
+
+            if(oddCnt%2!=0)
+            {
+                cout << "-1" << '\n';
+                return 0;
+            }
+        }
+    }
+
+    for(const auto& item: edges)
+    {
+        ropes.erase(ropes.find(item));
+        ropes.emplace(item.second, item.first);
+
+        ans.emplace_back(1, item.first, item.second);
+    }
+
+    for(auto item: ropes)
+    {
+        int u = item.first, v = item.second;
+
+        graph2[0][u].push_back(v);
+        graph2[1][v].push_back(u);
+    }
+
+    memset(used, 0, sizeof(used));
+    vector <pair <int, int>> components;
+
+    for(int x = 1;x<=n;x++)
+    {
+        if(used[1][x]==false)
+        {
+            vector <int> oddNodes;
+            int sz = dfs2(1, x, oddNodes);
+
+            if(sz>1)
+            {
+                while(oddNodes.size()>2)
+                {
+                    int x = oddNodes.back();oddNodes.pop_back();
+                    int y = oddNodes.back();oddNodes.pop_back();
+                    ans.emplace_back(2, x, y);
+                }
+
+                if(oddNodes.empty()==false)
+                    components.emplace_back(oddNodes[0], oddNodes[1]);
+                else
+                    components.emplace_back(x, x);
+            }
+        }
+    }
+
+    if(components.empty()==false)
+    {
+        if(components.size()>1)
+        {
+            for(int i = 0;i+1<components.size();i++)
+                ans.emplace_back(2, components[i].second, components[i+1].first);
+            ans.emplace_back(2, components.front().first, components.back().second);
+        }
+        else
+        {
+            if(components.front().first!=components.front().second)
+                ans.emplace_back(2, components.front().first, components.front().second);
+        }
+    }
+
+    cout << ans.size() << '\n';
+    for(const auto& item: ans)
+        cout << item.type << " " << item.i << " " << item.j << '\n';
+
+    return 0;
 }
